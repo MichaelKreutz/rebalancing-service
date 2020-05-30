45588adc0e5e4d2a9aaa783040a93d139e658812
Michael Kreutz <kreutz.michael@gmail.com>
2020-05-30 09:51:12 +0200

    config: initialize application.

---
+++ src/main/java/de/kreutz/michael/rebalanceservice/RebalanceServiceApplication.java
@@ -0,0 +1,13 @@
+package de.kreutz.michael.rebalanceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebalanceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RebalanceServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/kreutz/michael/rebalanceservice/RebalanceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kreutz.michael.rebalanceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RebalanceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
