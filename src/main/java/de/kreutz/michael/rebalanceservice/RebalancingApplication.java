aa71d4cde3316490141107131881d689b9a1b077
Michael Kreutz <kreutz.michael@gmail.com>
2020-05-31 22:48:13 +0200

    config: enable async.

--- src/main/java/de/kreutz/michael/rebalanceservice/RebalancingApplication.java
+++ src/main/java/de/kreutz/michael/rebalanceservice/RebalancingApplication.java
@@ -2,8 +2,10 @@ package de.kreutz.michael.rebalanceservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class RebalancingApplication {
 
     public static void main(String[] args) {
